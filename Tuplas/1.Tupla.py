730a14a9aa652df3d0824e20a04990e7906ce6d5
Edon Brendon Souza <e.brendon30@outlook.com>
2021-09-22 04:37:47 -0300

    Delete Tuplas directory

--- Tuplas/1.Tupla.py
+++
@@ -1,5 +0,0 @@
-lanche = ('pastel', 'coxinha', 'suco', 'pudim')  # Declaração de uma tupla
-print(type(lanche))  # Imprime o tipo da variável
-print(lanche)  # Imprime todo o conteúdo da tupla
-print(lanche[3])  # Imprime o índice 3 que equivale ao elemento 'pudim'
-print(lanche[:2])  # Imprime os elementos anteriores ao índice 2
--- Tuplas/2.Inserção_na_tupla.py
+++
@@ -1,4 +0,0 @@
-lanche = ('pastel', 'coxinha', 'suco', 'pudim')  # Declaração de uma tupla
-# A tupla é um elemento imutável em execução, logo não é possível fazer alterações
-lanche[1] = 'bolo'
-print(lanche)
